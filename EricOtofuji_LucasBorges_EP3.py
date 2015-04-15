d4a926d78b408586f98d8ce943fdab399932f299
Otofuji <ericfoa@al.insper.edu.br>
2015-04-15 11:01:01 -0300

    Um breve avanço

--- EricOtofuji_LucasBorges_EP3.py
+++ EricOtofuji_LucasBorges_EP3.py
@@ -5,3 +5,9 @@ Created on Wed Apr 15 09:55:20 2015
 @author: Eric & Lucas
 """
 
+print("Bem-vindo ao controle de dieta! Manter uma vida saudável é muito importante, e vamos te ajudar a chegar em seus objetivos. Vamos lá?")
+oal = open("alimentos(1).csv") #oal é o arquivo alimentos(1).csv aberto e apenas isso
+alim = oal.readlines #alim é variável que contém as linhas de informação de alimentos(1).csv
+#print(alim)
+ous = open("usuario(1).csv") #ous é o arquivo usuario(1).csv aberto e apenas isso
+dadosusuárioous.readline(2)
