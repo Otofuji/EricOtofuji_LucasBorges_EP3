8a2ac5770643c705795046168038bb3fbd29a56a
Otofuji <ericfoa@al.insper.edu.br>
2015-04-15 11:54:40 -0300

    Variáveis de nome e início de leitura dos demais

--- EricOtofuji_LucasBorges_EP3.py
+++ EricOtofuji_LucasBorges_EP3.py
@@ -10,5 +10,20 @@ oal = open("alimentos.csv","r", encoding="utf-8") #oal é o arquivo alimentos.cs
 alim = oal.readlines() #alim é variável que contém as linhas de informação de alimentos.csv
 #print(alim)
 ous = open("usuario.csv","r", encoding="utf-8") #ous é o arquivo usuario.csv aberto e apenas isso
-ous.readline()
+ous.readline() #linejumper
 datauser = ous.readline().split(",") #datauser são os dados básicos do usuário como nome, mas ainda não organizados.
+nome = datauser[0]
+idade = datauser[1]
+peso = datauser[2]
+sexo = datauser[3]
+altura = datauser[4]
+fator = datauser[5]
+"""
+As variáveis nas linhas 15-20 são exatamente o que seus nomes sugerem: os dados do usuário oriundos do arquivo usuario.csv.
+"""
+ous.readline() #linejumper
+cal == 0
+while not ous.readline()=="":
+    ous.readline()
+    
+    for i in (numeros)
