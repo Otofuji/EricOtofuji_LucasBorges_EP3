a129941d9594f50cc7a039f15c512d95ac18a1b1
Otofuji <ericfoa@al.insper.edu.br>
2015-04-16 11:31:59 -0300

    Limpeza e documentação
    Limpeza de lixos no código e documentação mais precisa, clara,
    especificada e detalhada, explicando linha a linha o que cada coisa faz
    e por que cada coisa faz isso e como

--- EricOtofuji_LucasBorges_EP3.py
+++ EricOtofuji_LucasBorges_EP3.py
@@ -1,88 +1,35 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Apr 15 09:55:20 2015
-
-@author: Eric & Lucas
+Arquivo mestre do programa de controle de dieta
+EP3 - Design de Software - 2015 - Insper, instituto de ensino e pesquisa
+Devs: Eric Otofuji & Lucas Borges
 """
-print("Bem-vindo ao controle de dieta! Manter uma vida saudável é muito importante, e vamos te ajudar a chegar em seus objetivos. Vamos lá?\n")
-#oal = open("alimentos.csv","r", encoding="utf-8") #oal é o arquivo alimentos.csv aberto e apenas isso
-#alim = oal.readlines() #alim é variável que contém as linhas de informação de alimentos.csv
-ous = open("usuario.csv","r", encoding="utf-8") #ous é o arquivo usuario.csv aberto e apenas isso
+print("Bem-vindo ao controle de dieta! Manter uma vida saudável é muito importante, e vamos te ajudar a chegar em seus objetivos. Vamos lá?\n") #um texto de boas vindas fofélico
+ous = open("usuario.csv","r", encoding="utf-8") #ous é o arquivo usuario.csv aberto e apenas isso - depois de aberto não vai servir para muita coisa, então não é algo a se preocupar muito.
 ous.readline() #linejumper
-datauser = ous.readline().split(",") #datauser são os dados básicos do usuário como nome, mas ainda não organizados.
-nome = datauser[0]
-idade = int(datauser[1])
-peso = int(datauser[2])
-sexo = datauser[3]
-altura = float(datauser[4])
-"""
-CÓDIGO INVÁLIDO USADO ANTERIORMENTE E DESCARTADO
-CONTINUA AQUI PARA EVENTUAL CONSULTA
-def fatorconvert(x):
-    if x=="mínimo":
-        return 0
-    elif x=="baixo":
-        return 1
-    elif x=="médio":
-        return 2
-    elif x=="alto":
-        return 3
-    elif x=="muito ativo":
-        return 4
-fator = fatorconvert(datauser[5])
-print(fator)
-"""
-fator = datauser[5]
-"""
-As variáveis nas linhas 15-20 são exatamente o que seus nomes sugerem: os dados do usuário oriundos do arquivo usuario.csv
-"""
-row = ous.readlines()
+datauser = ous.readline().split(",") #datauser são os dados básicos do usuário como nome, mas ainda não organizados. Esta linha também separa a linha em índices em uma lista separada por vírgulas. 
+nome = datauser[0] #variável "nome" é o índice zero (primeira coluna) de usuario.csv. Em suma, é o nome do dito-cujo.
+idade = int(datauser[1]) #variável "idade" éo índice 1 (segunda coluna) de usuario.csv. Em suma, é a idade do cidadão.
+peso = int(datauser[2]) #variável "peso" é o índice 2 (terceira coluna) de usuario.csv. Em suma, é o peso da criatura.
+sexo = datauser[3] #variável "sexo" é o índice 3 (quarta coluna) de usuario.csv. Em suma, é o sexo da pessoa.
+altura = float(datauser[4]) #variável "altura" é o índice 4 (quinta coluna) de usuario.csv. Em suma, é a altura do indivíduo.
+fator = datauser[5] ##variável "fator" é o índice 5 (sexta coluna) de usuario.csv. Em suma, é o fator de atividade física (o quanto se exercita) do usuário.
 ous.readline() #linejumper
-cal = 0 # quantidade de caloria
-
-"""
-CÓDIGO INVÁLIDO USADO ANTERIORMENTE E DESCARTADO
-CONTINUA AQUI PARA EVENTUAL CONSULTA
-datas1 = [] # Lista com as datas em que foram ingeridos os alimentos
-datas = []
-for i in range(1,len(row)):
-    alimentos = row[i].split(",")    
-    datas1.insert(0,alimentos[0]) # insere uma data na lista
-    datas1.sort(key=lambda x: x.split("/")[::-1]) # Organiza as datas em ordem crescente
-for i in datas1:
-    if i not in datas:
-       datas.insert(0,i)
-       datas.sort(key=lambda x: x.split("/")[::-1]) # Organiza as datas em ordem crescente
-x = 0
-while x <= len(datas):
-    for datas[x] in row:
-        print(row[2])
-    x+=1
-"""
-
-import csv
-with open("alimentos.csv", mode="r") as calref:
-    reader=csv.reader(calref)
-    calref={rows[0]:rows[2] for rows in reader}
-    """calref é dicionário com valores de referência do alimentos.csv"""
-with open("alimentos.csv", mode="r") as protref:
+row = ous.readlines() #lê todas as linhas do alimentos.csv daí pra baixo.
+cal = 0 #variável com quantidade de caloria em kcal
+prot = 0 #variável com quantidade de proteínas
+carb = 0 #variável com a quantidade de carboidratos
+gord = 0 #variável com a quantidade de gorduras
+import csv #importa a biblioteca csv, que será muito útil para as funções que usaremos para criar os dicionários com o banco de dados das informações nutricionais usando somente três linhas.
+with open("alimentos.csv", mode="r") as calref: #aqui estamos abrindo alimentos.csv para extrairmos os valores em dicionário de calorias e para tanto criamos a variável calref (referência de calorias)
+    reader=csv.reader(calref) #aqui usamos a função reader da biblioteca csv para ler o arquivo csv como csv e fazer o Python entender isso
+    calref={rows[0]:rows[2] for rows in reader} #aqui criamos dicionário calref do csv lido usando a coluna [0] como chaves e a coluna [2] como valores, que é a coluna que tem valores de calorias que é o que queremos aqui.
+with open("alimentos.csv", mode="r") as protref: #análogo às três linhas acima usando protref pois agora é a referência de proteínas
     reader=csv.reader(protref)
     protref={rows[0]:rows[3] for rows in reader}
-    """protref é o dicionário com valores de referência do alimentos.csv"""
-with open("alimentos.csv", mode="r") as carbref:
+with open("alimentos.csv", mode="r") as carbref: #análogo ao calref mas agora com carbref (referência de carboidratos).
     reader=csv.reader(carbref)
     carbref={rows[0]:rows[4] for rows in reader}
-    """carbref é o dicionário com os valores de referência do alimentos.csv"""
-with open("alimentos.csv", mode="r") as gordref:
+with open("alimentos.csv", mode="r") as gordref: #análogo ao calref mas agora com gordreg (referência de gorduras).
     reader=csv.reader(gordref)
     gordref={rows[0]:rows[5] for rows in reader}
-    """gordref é o dicionário com com os valores de referência do alimentos.csv"""
-with open("alimentos.csv", mode="r") as quant:
-    reader=csv.reader(quant)
-    quant={rows[0]:rows[1] for rows in reader}
-    """quant é o dicionário com as quantidades em gramas dos alimentos em alimentos.csv"""
-#calq={x:float(calref[x])/float(quant[x]) for x in calref}
-
-#print(calq["ACELGA"])
-
-
