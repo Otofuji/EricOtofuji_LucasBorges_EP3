0ff8a0c5df6ea9ae95fc74fc3c2a1c1201866473
Lucas Matheus Borges <lucasmatborges@gmail.com>
2015-04-16 20:03:51 -0300

    Função doctest
    Agora 4 das 9 funções definidas foram testadas pelo doctest

--- FuncoesUsuario.py
+++ FuncoesUsuario.py
@@ -16,14 +16,22 @@ sexo = datauser[3].upper().strip() # Lê e retorna o sexo(M ou F) do Usuário
 altura = float(datauser[4]) # Lê e retorna a altura(m) do Usuário
 fator = datauser[5].upper().strip() # Lê e retorna o fator do Usuário
 
-def CalculaNívelbasal(peso,altura,idade): # Calcula a Taxa de Metabolismo Basal(TMB)          
+def CalculaNívelbasal(peso,altura,idade): # Calcula a Taxa de Metabolismo Basal(TMB)                        
+    """ 
+    >>> round(CalculaNívelbasal(65,1.75,20),2) # Round seleciona quantas casas depois da virgula você quer exibir
+    1685.36
+    """    
     if sexo == "M": # No caso de sexo masculino
         TMB= 88.36+(13.4*peso)+(4.8*altura*100)-(5.7*idade)
     elif sexo == "F": # No caso de sexo feminino
-        TMB= 447.6+(9.2*peso)+(3.1*altura)-(4.3*idade)
+        TMB= 447.6+(9.2*peso)+(3.1*altura)-(4.3*idade)    
     return TMB
 
-def CalculaNecessidadeCal(fator): # Calcula a Necessidade calórica diária (kcal)
+def CalculaNecessidadeCal(fator): # Calcula a Necessidade calórica diária (kcal)    
+    """ 
+    >>> round(CalculaNecessidadeCal(fator),2) # Round seleciona quantas casas depois da virgula você quer exibir
+    2833.42
+    """
     if fator == "MÍNIMO":  # No caso de fator MÍNIMO
         TaxaCal = CalculaNívelbasal(peso,altura,idade)*1.2
     if fator == "BAIXO":    # No caso de fator BAIXO
@@ -35,23 +43,33 @@ def CalculaNecessidadeCal(fator): # Calcula a Necessidade calórica diária (kca
     if fator == "MUITO ALTO":    # No caso de fator MUITO ALTO
         TaxaCal = CalculaNívelbasal(peso,altura,idade)*1.9        
     return TaxaCal 
-
+print(CalculaNecessidadeCal(fator))
 def CalculaIMC(peso,altura): # Calcula o Índice de Massa Corporal (IMC)   
+    """ 
+    >>> round(CalculaIMC(50,1.5),2) # Round seleciona quantas casas depois da virgula você quer exibir
+    22.22
+    """        
     IMC= (peso)/(altura)**2  
     return IMC   
 
 def TipoIMC(peso,altura): # Define qual é o seu tipo de IMC
+    """ 
+    >>> round(TipoIMC(80,1.8),2) # Round seleciona quantas casas depois da virgula você quer exibir
+    Você está no peso ideal
+    2
+    """    
     if CalculaIMC(peso,altura)<18.5: # Caso esteja abaixo do peso
         print("Você está abaixo do peso")
+        return 1
     elif 18.5<=CalculaIMC(peso,altura)<25: # Caso esteja no peso ideal
         print("Você está no peso ideal")
+        return 2
     elif 25<=CalculaIMC(peso,altura)<30: # Caso esteja um pouco acima do peso
         print("Você está um pouco acima do peso")
+        return 3
     else:
         print("Você está muito acima do peso") # Caso esteja muito acima do peso
-    return None
-
-
+        return 4
          
 """ Dicionários com a quantidade de cada alimento ingeridos"""    
 SemanaGramas={}
@@ -115,3 +133,7 @@ def usuariofatcsv():
             else:
                 SemanaGorduras[data]=gordref[comida]*gramas  
     return SemanaGorduras 
+    
+if __name__ =='__main__':	
+    import doctest
+    doctest.testmod(verbose=True)
